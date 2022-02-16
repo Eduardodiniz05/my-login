c1e10237c697f30f179694204329c39457f9d9d6
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-02-16 15:49:39 -0300

    adicionado os armazenamentos da pag de login

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,18 +1,39 @@
 import React from "react";
 import './styles.css';
+import { useState } from 'react';
+import { clear } from "@testing-library/user-event/dist/clear";
 
 const LoginPage = () => {
-    return(
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState('');
+    const handleSubmit = (e) => {
+        //interceptando o browser de fazer a ação padrão de atualiza pag
+        e.preventDefault();
+        console.log('submit', {email, password})
+    };
+    return (
         <div id="login">
             <h1 className="title">Login do Sistema</h1>
-            <form className="form">
+            <form className="form" onSubmit={handleSubmit}>
                 <div className="field">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="email" />
+                    <input
+                        type="email"
+                        name="email"
+                        id="email"
+                        required
+                        value={email} onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
                 <div className="field">
                     <label htmlFor="password">Senha</label>
-                    <input type="password" name="password" id="password" />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        required
+                        value={password} onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div className="actions">
                     <button type="submit">Entrar</button>
@@ -20,7 +41,7 @@ const LoginPage = () => {
             </form>
         </div>
     )
-};  
+};
 
 
 export default LoginPage;
