61d6125a00406cc9be0a7e6cce395cef4ab93556
eduardodiniz05 <eduardo.diniz005@gmail.com>
2022-02-22 10:39:59 -0300

    Terminado a parte frontal da aplicação de login

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,19 +1,37 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, useEffect, createContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
     const navigate = useNavigate();
-
     const [user, setUser] = useState(null);
+    const[loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const recoveredUser = localStorage.getItem('user');
+
+        if(recoveredUser){
+            setUser(JSON.parse(recoveredUser))
+        }
+
+
+        setLoading(false);
+    }, []);
 
     const login = (email, password) => {
         
         console.log('login Auth', {email, password});
 
+        const loggedUser = {
+            id: "123", 
+            email,
+        };
+
+        localStorage.setItem('user', JSON.stringify(loggedUser));
+
         if (password == '123') {
-            setUser({id: "123", email} );
+            setUser(loggedUser);
             navigate('/')
         };
 
@@ -21,11 +39,12 @@ export const AuthProvider = ({children}) => {
 
     const logout = () => {
         console.log('logout');
+        localStorage.removeItem('user');
         setUser(null);
         navigate('/login')
     };
 
     return(
-        <AuthContext.Provider value={{authenticated: !!user, user, login, logout}}>{children}</AuthContext.Provider>
+        <AuthContext.Provider value={{authenticated: !!user, user, loading, login, logout}}>{children}</AuthContext.Provider>
     )
 }
